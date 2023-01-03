baa24365ce92b935debe2c988d28e525b812e303
Terri Archer <territangela@gmail.com>
2023-01-03 14:15:05 -0500

    Began rebuilding personal project

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+const Header = (props) => {
+
+    return (
+        <div>
+            <h1>This is Rebecca Guay</h1>
+        </div>
+    )
+}
+
+
+export default Header;
